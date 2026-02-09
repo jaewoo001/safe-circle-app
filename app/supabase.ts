38cc4e15d561d4c7d59b60a08d61af9f3313a348
jaewoo001 <97401957+jaewoo001@users.noreply.github.com>
2026-02-09 16:14:09 +0800

    Login Update

--- app/supabase.ts
+++ app/supabase.ts
@@ -1,6 +1,14 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+// If these are missing, the app will throw that error
+if (!supabaseUrl || !supabaseKey) {
+  console.error("Missing Supabase Environment Variables!");
+}
+
+export const supabase = createClient(
+  supabaseUrl || '', 
+  supabaseKey || ''
+);
